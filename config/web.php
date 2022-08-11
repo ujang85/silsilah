92327e47d5d807f2ef7a30a398a08aa309f93335
System Administrator <root@muhammads-MacBook-Pro.local>
2022-08-11 12:52:48 +0700

    hilangkan  pretty url

--- config/web.php
+++ config/web.php
@@ -43,14 +43,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        
+       /* 
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => ['yii\rest\UrlRule',
             'controller'=>'silsilah-rest'
             ],
-        ],
+        ], */
         
     ],
     'params' => $params,
